7deaabc2a2d9fedb7df08d69b94bbc48526e1aa1
hooiyan <hoecoding@gmail.com>
2022-03-18 19:51:01 +0800

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useState } from "react"
 import { ThemeProvider } from "styled-components"
 import AppHeader from "./components/AppHeader"
 import AddTodo from "./components/AddTodo"
