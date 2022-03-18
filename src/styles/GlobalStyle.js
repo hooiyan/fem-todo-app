b761a17e71fe9bfc34c51b62aeacd118c3dec102
hooiyan <hoecoding@gmail.com>
2022-03-18 19:56:07 +0800

    Modify transition-property

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,7 +7,8 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-  transition: all 0.3s;
+  transition-property: color, background, border, border-bottom;
+  transition-duration: 0.3s;
 }
 
 :root {
