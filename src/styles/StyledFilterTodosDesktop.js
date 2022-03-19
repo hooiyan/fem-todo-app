5ce5e38ae566fc558a1836386aa4a294e7a4c39e
hooiyan <hoecoding@gmail.com>
2022-03-19 08:57:23 +0800

    Make the font bold

--- src/styles/StyledFilterTodosDesktop.js
+++ src/styles/StyledFilterTodosDesktop.js
@@ -6,7 +6,6 @@ export const StyledFilterTodoDesktop = styled.section`
   @media only screen and (min-width: 600px) {
     display: flex;
     justify-content: space-between;
-    color: ${(props) => props.theme.todolistBottom};
     font-size: 1.2rem;
     font-weight: var(--font-weight-bold);
     width: 166px;
@@ -15,13 +14,15 @@ export const StyledFilterTodoDesktop = styled.section`
   @media only screen and (min-width: 990px) {
     font-size: 1.4rem;
   }
+  
+  .option {
+    color: ${(props) => props.theme.todolistBottom};
+    font-weight: var(--font-weight-bold);
+    cursor: pointer;
+  }
 
   .selected {
     color: var(--primary-bright-blue);
-  }
-
-  .option {
-    cursor: pointer;
   }
 
   .option:hover {
