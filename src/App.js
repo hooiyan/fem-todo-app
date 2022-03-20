cfcf479d7d2b0d826c9aa1de7fdaa413307cdd4e
hooiyan <hoecoding@gmail.com>
2022-03-20 11:27:21 +0800

    Implement prefers-color-scheme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ThemeProvider } from "styled-components"
 import AppHeader from "./components/AppHeader"
 import AddTodo from "./components/AddTodo"
@@ -14,33 +14,28 @@ import IconSun from "./images/icon-sun.svg"
 import { themes } from "./context/themeContext"
 
 export default function App() {
-  // const [theme, setTheme] = useState("light");
-  // const isDarkTheme = theme === "dark";
-  
-  // const toggleTheme = () => {
-  //   const updatedTheme = isDarkTheme ? "light" : "dark";
-  //   setTheme(updatedTheme);
-  //   localStorage.setItem("theme", updatedTheme);
-  // };
-  // useEffect(() => {
-  //   const savedTheme = localStorage.getItem("theme");
-  //   const prefersDark = window.matchMedia &&
-  //     window.matchMedia('(prefers-color-scheme: dark)').matches;
-  //   if (savedTheme && ["dark", "light"].includes(savedTheme)) {
-  //     setTheme(savedTheme);
-  //   } else if (prefersDark) {
-  //     setTheme("dark");
-  //   }
-  // }, []);
-
   const { light, dark } = themes
+  const [theme, setTheme] = useState("light")
+  const isDarkTheme = theme === "dark"
 
-  const [isLightMode, setTheme] = useState(false)
   const toggleTheme = () => {
-    // const updatedtheme = isLightMode ? light : dark
-    // setTheme(updatedtheme)
-    setTheme(!isLightMode)
+    const updatedTheme = isDarkTheme ? "light" : "dark"
+    setTheme(updatedTheme)
+    localStorage.setItem("theme", updatedTheme)
   }
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme")
+    const prefersDark =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+
+    if (savedTheme && ["dark", "light"].includes(savedTheme)) {
+      setTheme(savedTheme)
+    } else if (prefersDark) {
+      setTheme("dark")
+    }
+  }, [])
 
   const showAll = () => {
     console.log("show all")
@@ -55,12 +50,12 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={isLightMode ? light : dark}>
+    <ThemeProvider theme={isDarkTheme ? dark : light}>
       <GlobalStyle />
       <StyledWrapper>
         <AppHeader
           toggleTheme={toggleTheme}
-          icon={isLightMode ? IconMoon : IconSun}
+          icon={isDarkTheme ? IconSun : IconMoon}
         />
         <main>
           <AddTodo />
